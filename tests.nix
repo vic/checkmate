e7b014c2e1e19bb9442007c9386fad6ab656ee6c
Victor Borja <vborja@apache.org>
2025-03-14 17:29:29 -0600

    Remove tests from default config

--- tests.nix
+++ tests.nix
@@ -22,7 +22,6 @@
       nix-unit.inputs = fixedInputs;
       # Tests are defined by target, and imported bellow by `inputs.target.flakeModules.nix-unit`.
       # See example/flake.nix or https://github.com/vic/import-tree/tree/main/checks/default.nix
-      nix-unit.tests = { };
     }
   );
 }
